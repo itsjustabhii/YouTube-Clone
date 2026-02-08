1895d960773b7395518061c13e916be2f500bba7
Abhishek Harish <itsjustabhii@gmail.com>
2026-02-08 13:11:46 +0530

    mounted /video route path

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/video/:categoryId/:videoId' element={<Video/>}/>
       </Routes>
     </div>
   )
