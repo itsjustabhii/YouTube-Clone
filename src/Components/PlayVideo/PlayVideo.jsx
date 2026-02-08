5a5a8ce966e81f09f78d4f85cd36f0b820a1edfa
Abhishek Harish <itsjustabhii@gmail.com>
2026-02-08 18:36:52 +0530

    added playvideo component

---
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import './PlayVideo.css'
+import video from '../../assets/video.mp4'
+import like from '../../assets/like.png'
+import dislike from '../../assets/dislike.png'
+import share from '../../assets/share.png'
+import save from '../../assets/save.png'
+import jack from '../../assets/jack.png'
+import user_profile from '../../assets/user_profile.jpg'
+
+const PlayVideo = () => {
+  return (
+    <div className='play-video'>
+        <video src={video} controls autoPlay muted></video>
+        <h3> Best YouTube channel to learn web development</h3>
+        <div className="play-video-info">
+            <p>1568 views &bull; 2 days ago</p>
+            <div>
+                <span><img src={like} alt="" />125</span>
+                <span><img src={dislike} alt="" />2</span>
+                <span><img src={share} alt="" />Share</span>
+                <span><img src={save} alt="" />Save</span>
+            </div>
+        </div>
+        <hr />
+        <div className="publisher">
+            <img src={jack} alt=""  />
+            <div>
+                <p>MrBeast</p>
+                <span>94M Subscribers</span>
+            </div>
+            <button>Subscribe</button>
+        </div>
+        <div className="vid-description">
+            <p>Channel that makes you stay awake</p>
+            <p>Subscribe for more fun</p>
+            <hr />
+            <h4>156 Comments</h4>
+            <div className="comment">
+                <img src={user_profile} alt=""  />
+                <div>
+                    <h3>Jack Henderson <span>1 day ago</span></h3>
+                    <p>A global network of content creators fighting for survival in Jake Paul's Ranch in Texas, USA</p>
+                    <div className="comment-action">
+                        <img src={like} alt=""  />
+                        <span>244</span>
+                        <img src={dislike} alt="" />
+                    </div>
+                </div>
+            </div>
+            <div className="comment">
+                <img src={user_profile} alt=""  />
+                <div>
+                    <h3>Jack Henderson <span>1 day ago</span></h3>
+                    <p>A global network of content creators fighting for survival in Jake Paul's Ranch in Texas, USA</p>
+                    <div className="comment-action">
+                        <img src={like} alt=""  />
+                        <span>244</span>
+                        <img src={dislike} alt="" />
+                    </div>
+                </div>
+            </div>
+            <div className="comment">
+                <img src={user_profile} alt=""  />
+                <div>
+                    <h3>Jack Henderson <span>1 day ago</span></h3>
+                    <p>A global network of content creators fighting for survival in Jake Paul's Ranch in Texas, USA</p>
+                    <div className="comment-action">
+                        <img src={like} alt=""  />
+                        <span>244</span>
+                        <img src={dislike} alt="" />
+                    </div>
+                </div>
+            </div>
+            <div className="comment">
+                <img src={user_profile} alt=""  />
+                <div>
+                    <h3>Jack Henderson <span>1 day ago</span></h3>
+                    <p>A global network of content creators fighting for survival in Jake Paul's Ranch in Texas, USA</p>
+                    <div className="comment-action">
+                        <img src={like} alt=""  />
+                        <span>244</span>
+                        <img src={dislike} alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default PlayVideo
