50faaa7d0a5f59a96fcbc616fa79c43e4548619b
Abhishek Harish <itsjustabhii@gmail.com>
2026-02-08 12:49:36 +0530

    Mounted Route path / for Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
     </div>
   )
 }
